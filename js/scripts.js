57e34f82608edc16d109373be221399dfa72a49f
natig agharzayev <87902339+natigGit@users.noreply.github.com>
2022-11-09 15:29:21 +0400

    modify header section and burger

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,19 @@ function burgerMenu() {
     const burger = document.querySelector('.burger')
     const menu = document.querySelector('.menu')
     const body = document.querySelector('body')
+
+    menu.addEventListener('click', (event) =>{
+      if (!menu.classList.contains('active')) {
+        menu.classList.add('active')
+        burger.classList.add('active-burger')
+        body.classList.add('locked')
+      } else {
+        menu.classList.remove('active')
+        burger.classList.remove('active-burger')
+        body.classList.remove('locked')
+      }
+    });
+
     burger.addEventListener('click', () => {
       if (!menu.classList.contains('active')) {
         menu.classList.add('active')
